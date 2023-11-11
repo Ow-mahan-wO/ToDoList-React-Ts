509e5033d2bb8314dec1d6202ddaa87d391d79dd
Ow-mahan-wO <112275799+Ow-mahan-wO@users.noreply.github.com>
2023-11-12 00:36:54 +0330

    debuging Complete Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React,{useState} from 'react';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-import { TodoList } from './Components/Todo/Todo'
-import { NewTodo } from './Components/NewTodo/NewTodo'
-import { TodoArray } from './Todo.Model'
+import { TodoList } from "./Components/Todo/Todo";
+import { NewTodo } from "./Components/NewTodo/NewTodo";
+import { TodoArray } from "./Todo.Model";
 
-let TodoId=0
-const App:React.FC=()=> {
-  const [Todos,setTodo]=useState<TodoArray[]>([]);
-  localStorage.setItem("value", JSON.stringify(Todos));
-  
-  
-  const AddTodoHandler=(Todo:string)=>{
-    setTodo([...Todos,{id:TodoId++,Todo:Todo,Status:false}])
-    localStorage.setItem('value',JSON.stringify(Todos))
-  }
+let TodoId = 0;
+const App: React.FC = () => {
+  const [Todos, setTodo] = useState<TodoArray[]>([]);
+
+  const AddTodoHandler = (Todo: string) => {
+    Todos.push({ id: TodoId++, Todo: Todo, Status: false });
+    setTodo(()=>[...Todos,{ id: TodoId++, Todo: Todo, Status: false }])
+    localStorage.setItem("value", JSON.stringify(Todos));
+  };
   
   return (
     <>
-     <NewTodo onAddTodo={AddTodoHandler} />
-     <TodoList Item={JSON.parse(localStorage.getItem('value')!)||Todos} setTodo={setTodo}/>
+      <NewTodo onAddTodo={AddTodoHandler} />
+      <TodoList
+        Item={JSON.parse(localStorage.getItem("value")!) || Todos}
+        setTodo={setTodo}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
