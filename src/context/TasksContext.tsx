6675218fa47c4b27ac83679a96d8094960e946b4
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2024-08-11 13:29:56 +0330

    doc: adding comments to the context and its custom hook

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useState, useEffect, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
 import { Task } from "../models/task.model";
 
 type TasksContextType = {
@@ -10,44 +16,54 @@ type TasksContextType = {
 
 const TasksContext = createContext<TasksContextType | undefined>(undefined);
 
+// Providing the tasks context to the application.
 export const TasksProvider = ({ children }: { children: ReactNode }) => {
+
+  // Load tasks from local storage or set an empty array if there are no tasks.
   const [tasks, setTasks] = useState<Task[]>(() => {
-	const savedTasks = localStorage.getItem("ow-mahan-wo-todo-list-tasks");
-	return JSON.parse(savedTasks!) ?? [];
+    const savedTasks = localStorage.getItem("ow-mahan-wo-todo-list-tasks");
+    return JSON.parse(savedTasks!) ?? [];
   });
 
+  // Save tasks to local storage whenever tasks change.
   useEffect(() => {
-	localStorage.setItem("ow-mahan-wo-todo-list-tasks", JSON.stringify(tasks));
+    localStorage.setItem("ow-mahan-wo-todo-list-tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  // Add a new task to the list of tasks.
   const addTask = (title: string) => {
-	const newTask: Task = { id: tasks.length, title, status: false };
-	setTasks((prevTasks) => [...prevTasks, newTask]);
+    const newTask: Task = { id: tasks.length, title, status: false };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
   };
 
+  // Remove a task from the list of tasks.
   const removeTask = (id: number) => {
-	const updatedTasks = tasks.filter((task) => task.id !== id);
-	setTasks(updatedTasks);
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
   };
 
+  // Mark a task as complete.
   const markTaskAsComplete = (id: number) => {
-	const updatedTasks = tasks.map((task) =>
-	  task.id === id ? { ...task, status: true } : task
-	);
-	setTasks(updatedTasks);
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, status: true } : task
+    );
+    setTasks(updatedTasks);
   };
 
   return (
-	<TasksContext.Provider value={{ tasks, addTask, removeTask, markTaskAsComplete }}>
-	  {children}
-	</TasksContext.Provider>
+    <TasksContext.Provider
+      value={{ tasks, addTask, removeTask, markTaskAsComplete }}
+    >
+      {children}
+    </TasksContext.Provider>
   );
 };
 
+// Custom hook to use the tasks context.
 export const useTasks = () => {
   const context = useContext(TasksContext);
   if (!context) {
-	throw new Error("useTasks must be used within a TasksProvider");
+    throw new Error("useTasks must be used within a TasksProvider");
   }
   return context;
 };
