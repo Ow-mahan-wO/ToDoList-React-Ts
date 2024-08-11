be009ae4af4aecf7386ac749f182be10223f88ca
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2024-08-11 12:55:56 +0330

    refactor: enhance the `Tools.tsx`.

--- src/Components/Tools/Tools.tsx
+++ src/Components/Tools/Tools.tsx
@@ -1,36 +1,39 @@
-import { memo, useRef } from "react";
+import React, {  memo, useRef } from "react";
 import "./Tools.css";
 
 type ToolsProps = {
   onNewTask: (Todo: string) => void;
 }
 
-const Tools = memo((props: ToolsProps) => {
+const Tools = memo(({ onNewTask }: ToolsProps) => {
   const taskTitleRef = useRef<HTMLInputElement>(null);
 
-  const handleNewTask = () => {
-    const newTaskTitle = taskTitleRef.current!.value;
-    props.onNewTask(newTaskTitle);
+  const handleNewTask = (): void => {
+    const newTaskTitle = taskTitleRef.current?.value;
+    if (newTaskTitle) {
+      onNewTask(newTaskTitle);
+      taskTitleRef.current.value = "";
+    }
+  };
 
-    taskTitleRef.current!.value = "";
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === "Enter") {
+      handleNewTask();
+    }
   };
 
   return (
     <div className="tools-container">
-      <div className="input-container">
+      <div className="tools-container__input-container">
         <input
           className="tools-container__input"
           placeholder="Write your new task!"
           type="text"
           ref={taskTitleRef}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleNewTask();
-            }
-          }}
+          onKeyDown={handleKeyDown}
         />
       </div>
-      <div className="button-container">
+      <div className="tools-container__button-container">
         <button
           className="tools-container__add-button"
           type="submit"
